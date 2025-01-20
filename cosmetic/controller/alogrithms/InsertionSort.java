4adc6873fcff04cffc33290a4e0adc893b672c74
AaditiG21 <155234805+AaditiG21@users.noreply.github.com>
2025-01-20 10:15:46 +0545

    Update InsertionSort.java

--- cosmetic/controller/alogrithms/InsertionSort.java
+++ cosmetic/controller/alogrithms/InsertionSort.java
@@ -10,79 +10,115 @@ import java.util.ArrayList;
 
 /**
  * LMUID: 23048679
- * @author ASUS1
+ *
+ * @author Aaditi Ghimire
  */
 public class InsertionSort {
-     public void ExecuteInsertionSortByNameA( ArrayList <MakeupModel> makeupList){
-        
+
+    /**
+     * Sorts the given list of MakeupModel objects in ascending order based on
+     * their names.
+     *
+     * @param makeupList The list of MakeupModel objects to be sorted. The list
+     * will be sorted in-place, meaning the original list will be modified.
+     */
+    public void ExecuteInsertionSortByNameA(ArrayList<MakeupModel> makeupList) {
+
         int listSize = makeupList.size();
-        
-        for(int i=1; i<listSize; i++ ) {
+
+        for (int i = 1; i < listSize; i++) {
             MakeupModel temp = makeupList.get(i);
-            int j = i - 1; 
-            
-           while(j>=0 && makeupList.get(j).getMname().compareTo(temp.getMname())>0){
-               makeupList.set(j+1, makeupList.get(j));
-               j--;
-           
-           }
-            
-           makeupList.set(j+1, temp);
-            
+            int j = i - 1;
+
+            while (j >= 0 && makeupList.get(j).getMname().compareTo(temp.getMname()) > 0) {
+                makeupList.set(j + 1, makeupList.get(j));
+                j--;
+
+            }
+
+            makeupList.set(j + 1, temp);
+
         }
     }
-    public void ExecuteInsertionSortByNameD( ArrayList <MakeupModel> makeupList){
-        
+
+    public void ExecuteInsertionSortByNameD(ArrayList<MakeupModel> makeupList) {
+
+        /**
+         * Sorts the given list of MakeupModel objects in descending order based
+         * on their names.
+         *
+         * @param makeupList The list of MakeupModel objects to be sorted. The
+         * list will be sorted in-place, meaning the original list will be
+         * modified.
+         */
         int listSize = makeupList.size();
-        
-        for(int i=1; i<listSize; i++ ) {
+
+        for (int i = 1; i < listSize; i++) {
             MakeupModel temp = makeupList.get(i);
-            int j = i - 1; 
-            
-           while(j>=0 && makeupList.get(j).getMname().compareTo(temp.getMname())<0){
-               makeupList.set(j+1, makeupList.get(j));
-               j--;
-           
-           }
-            
-           makeupList.set(j+1, temp);
-            
+            int j = i - 1;
+
+            while (j >= 0 && makeupList.get(j).getMname().compareTo(temp.getMname()) < 0) {
+                makeupList.set(j + 1, makeupList.get(j));
+                j--;
+
+            }
+
+            makeupList.set(j + 1, temp);
+
         }
     }
-      public void ExecuteInsertionSortByNameFA( ArrayList <FragnanceModel> fragnanceList){
-        
+
+    public void ExecuteInsertionSortByNameFA(ArrayList<FragnanceModel> fragnanceList) {
+
+        /**
+         * Sorts the given list of FragnanceModel objects in ascending order
+         * based on their names.
+         *
+         * @param fragnanceList The list of FragnanceModel objects to be sorted.
+         * The list will be sorted in-place, meaning the original list will be
+         * modified.
+         */
         int listSize = fragnanceList.size();
-        
-        for(int i=1; i<listSize; i++ ) {
+
+        for (int i = 1; i < listSize; i++) {
             FragnanceModel temp = fragnanceList.get(i);
-            int j = i - 1; 
-            
-           while(j>=0 && fragnanceList.get(j).getFname().compareTo(temp.getFname())>0){
-               fragnanceList.set(j+1, fragnanceList.get(j));
-               j--;
-           
-           }
-            
-          fragnanceList.set(j+1, temp);
-            
+            int j = i - 1;
+
+            while (j >= 0 && fragnanceList.get(j).getFname().compareTo(temp.getFname()) > 0) {
+                fragnanceList.set(j + 1, fragnanceList.get(j));
+                j--;
+
+            }
+
+            fragnanceList.set(j + 1, temp);
+
         }
     }
-    public void ExecuteInsertionSortByNameFD( ArrayList <FragnanceModel> fragnanceList){
-        
+
+    public void ExecuteInsertionSortByNameFD(ArrayList<FragnanceModel> fragnanceList) {
+
+        /**
+         * Sorts the given list of FragnanceModel objects in descending order
+         * based on their names.
+         *
+         * @param fragnanceList The list of FragnanceModel objects to be sorted.
+         * The list will be sorted in-place, meaning the original list will be
+         * modified.
+         */
         int listSize = fragnanceList.size();
-        
-        for(int i=1; i<listSize; i++ ) {
+
+        for (int i = 1; i < listSize; i++) {
             FragnanceModel temp = fragnanceList.get(i);
-            int j = i - 1; 
-            
-           while(j>=0 && fragnanceList.get(j).getFname().compareTo(temp.getFname())<0){
-               fragnanceList.set(j+1, fragnanceList.get(j));
-               j--;
-           
-           }
-            
-           fragnanceList.set(j+1, temp);
-            
+            int j = i - 1;
+
+            while (j >= 0 && fragnanceList.get(j).getFname().compareTo(temp.getFname()) < 0) {
+                fragnanceList.set(j + 1, fragnanceList.get(j));
+                j--;
+
+            }
+
+            fragnanceList.set(j + 1, temp);
+
         }
     }
 }
