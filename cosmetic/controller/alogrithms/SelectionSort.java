762b98809ac0e0045e6897e19fe9e484d69ac357
AaditiG21 <155234805+AaditiG21@users.noreply.github.com>
2025-01-20 10:22:53 +0545

    Update SelectionSort.java

--- cosmetic/controller/alogrithms/SelectionSort.java
+++ cosmetic/controller/alogrithms/SelectionSort.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package  com.cosmetic.controller.alogrithms;
+package com.cosmetic.controller.alogrithms;
 
 import com.cosmetic.model.FragnanceModel;
 import com.cosmetic.model.MakeupModel;
@@ -10,99 +10,110 @@ import java.util.ArrayList;
 
 /**
  * LMUID: 23048679
- * @author ASUS1
+ *
+ * @author Aaditi Ghimire
  */
 public class SelectionSort {
-    public void ExecuteSelectionSortA(ArrayList <MakeupModel> makeupList){
-        
+
+    /**
+     * Sorts a list of MakeupModel objects in ascending order based on the
+     * price.
+     *
+     * @param makeupList The list of MakeupModel objects to be sorted.
+     */
+    public void ExecuteSelectionSortA(ArrayList<MakeupModel> makeupList) {
         int listSize = makeupList.size();
-        
-        for(int step=0; step < listSize -1; step++) {
+        for (int step = 0; step < listSize - 1; step++) {
             int minIndex = step;
-            
-            for(int next=step+1; next < listSize; next++){
-                if(makeupList.get(next).getMprice() <
-                        makeupList.get(minIndex).getMprice()){
+
+            for (int next = step + 1; next < listSize; next++) {
+                if (makeupList.get(next).getMprice()
+                        < makeupList.get(minIndex).getMprice()) {
                     minIndex = next;
                 }
             }
-            
+
             MakeupModel tempMakeupModel = makeupList.get(step);
             makeupList.set(step, makeupList.get(minIndex));
-            makeupList.set(minIndex,tempMakeupModel);
+            makeupList.set(minIndex, tempMakeupModel);
         }
-        
-        
-    } 
-    public void ExecuteSelectionSortD( ArrayList <MakeupModel> makeupList){
-        
+    }
+
+    /**
+     * Sorts a list of MakeupModel objects in descending order based on the
+     * price.
+     *
+     * @param makeupList The list of MakeupModel objects to be sorted.
+     */
+    public void ExecuteSelectionSortD(ArrayList<MakeupModel> makeupList) {
+
         int listSize = makeupList.size();
-        
-        for(int step=0; step < listSize -1; step++) {
+
+        for (int step = 0; step < listSize - 1; step++) {
             int maxIndex = step;
-            
-            for(int next=step+1; next<listSize; next++){
-                if(makeupList.get(next).getMprice() >
-                        makeupList.get(maxIndex).getMprice()){
+
+            for (int next = step + 1; next < listSize; next++) {
+                if (makeupList.get(next).getMprice()
+                        > makeupList.get(maxIndex).getMprice()) {
                     maxIndex = next;
                 }
             }
-            
+
             MakeupModel tempMakeupModel = makeupList.get(step);
             makeupList.set(step, makeupList.get(maxIndex));
-            makeupList.set(maxIndex,tempMakeupModel);
-        }   
-        
+            makeupList.set(maxIndex, tempMakeupModel);
+        }
     }
-    
-   
-    
-   
-    
-    
-     public void ExecuteSelectionSortFA( ArrayList <FragnanceModel> fragnanceList){
-        
+
+    /**
+     * Sorts a list of FragnanceModel objects in ascending order based on the
+     * price.
+     *
+     * @param fragnanceList The list of FragnanceModel objects to be sorted.
+     */
+
+    public void ExecuteSelectionSortFA(ArrayList<FragnanceModel> fragnanceList) {
         int listSize = fragnanceList.size();
-        
-        for(int step=0; step < listSize -1; step++) {
+        for (int step = 0; step < listSize - 1; step++) {
             int minIndex = step;
-            
-            for(int next=step+1; next < listSize; next++){
-                if( fragnanceList.get(next).getFprice() <
-                         fragnanceList.get(minIndex).getFprice()){
+
+            for (int next = step + 1; next < listSize; next++) {
+                if (fragnanceList.get(next).getFprice()
+                        < fragnanceList.get(minIndex).getFprice()) {
                     minIndex = next;
                 }
             }
-            
+
             FragnanceModel tempFragnanceModel = fragnanceList.get(step);
-            fragnanceList.set(step,  fragnanceList.get(minIndex));
-             fragnanceList.set(minIndex,tempFragnanceModel);
+            fragnanceList.set(step, fragnanceList.get(minIndex));
+            fragnanceList.set(minIndex, tempFragnanceModel);
         }
-        
-        
-    } 
-    public void ExecuteSelectionSortFD( ArrayList <FragnanceModel> fragnanceList){
-        
+
+    }
+
+    /**
+     * Sorts a list of FragnanceModel objects in descending order based on the
+     * price.
+     *
+     * @param fragnanceList The list of FragnanceModel objects to be sorted.
+     */
+    public void ExecuteSelectionSortFD(ArrayList<FragnanceModel> fragnanceList) {
+
         int listSize = fragnanceList.size();
-        
-        for(int step=0; step < listSize -1; step++) {
+
+        for (int step = 0; step < listSize - 1; step++) {
             int maxIndex = step;
-            
-            for(int next=step+1; next<listSize; next++){
-                if(fragnanceList.get(next).getFprice() >
-                        fragnanceList.get(maxIndex).getFprice()){
+
+            for (int next = step + 1; next < listSize; next++) {
+                if (fragnanceList.get(next).getFprice()
+                        > fragnanceList.get(maxIndex).getFprice()) {
                     maxIndex = next;
                 }
             }
-            
+
             FragnanceModel tempFragnanceModel = fragnanceList.get(step);
             fragnanceList.set(step, fragnanceList.get(maxIndex));
-            fragnanceList.set(maxIndex,tempFragnanceModel);
-        }  
-        
+            fragnanceList.set(maxIndex, tempFragnanceModel);
+        }
     }
-      
-
-    
 }
-
